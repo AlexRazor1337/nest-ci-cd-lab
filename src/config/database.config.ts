06d79ae69b373b8b2cff7641344404bf9a0a2f57
AlexRazor <alexrazor1337@gmail.com>
2024-10-20 22:00:03 +0300

    Fix ssl, add env file reading for dev

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,8 +1,11 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-
+console.log(process.env.DATABASE_URL);
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true, // Set to false in production
+  ssl: {
+    rejectUnauthorized: false,
+  }
 };
