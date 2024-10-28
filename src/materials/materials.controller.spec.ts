228ff8d75f3e67ea696148acfeb1c4fb58ac9308
AlexRazor <alexrazor1337@gmail.com>
2024-10-28 02:37:51 +0200

    Fix sample tests

--- src/materials/materials.controller.spec.ts
+++ src/materials/materials.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MaterialsController } from './materials.controller';
+import { MaterialsService } from './materials.service';
 
 describe('MaterialsController', () => {
   let controller: MaterialsController;
@@ -7,6 +8,10 @@ describe('MaterialsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MaterialsController],
+      providers: [{
+        provide: MaterialsService,
+        useValue: {},
+      }],
     }).compile();
 
     controller = module.get<MaterialsController>(MaterialsController);
